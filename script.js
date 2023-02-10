339f3ecb2fe56b1aa15787911a5351134a52c9a3
Alexandra July <catmon0201@gmail.com>
2023-02-10 12:06:03 +0200

    t

---
+++ script.js
@@ -0,0 +1,93 @@
+const declaredVariable;
+const someData = 99;
+const obj = {age: 33};
+
+typeof 37 === "number"
+console.log(typeof 37);
+
+typeof obj === 'object'
+console.log(typeof obj);
+
+typeof true === 'boolean'
+console.log(typeof true );
+
+typeof 3.14 === 'number'
+console.log(typeof 3.14);
+
+typeof obj.name === 'undefined'
+console.log(typeof obj.name);
+
+typeof 42 === 'number'
+console.log(typeof 42);
+
+typeof "" === 'string'
+console.log(typeof "");
+
+typeof "bla" === 'string'
+console.log(typeof "bla");
+
+typeof Boolean(1) === 'boolean'
+console.log(typeof Boolean(1));
+
+typeof !!1 === 'boolean'
+console.log(typeof !!1);
+
+typeof obj.age === 'number'
+console.log(typeof obj.age);
+
+typeof Infinity === 'number'
+console.log(typeof Infinity);
+
+typeof NaN === 'number' 
+console.log(typeof NaN);
+
+typeof (someData + " Wisen") === 'string';
+console.log(typeof (someData + " Wisen"));
+
+typeof Number("1") === 'number'
+console.log(typeof Number("1"));
+
+typeof Number("shoe") === 'number'
+console.log(typeof Number("shoe"));
+
+typeof `template literal` === 'string'
+console.log(typeof `template literal`);
+
+typeof "1" === 'string'
+console.log(typeof "1");
+
+typeof typeof 1 === 'string'
+console.log(typeof typeof 1);
+
+typeof false === 'boolean'
+console.log(typeof false);
+
+typeof String(1) === 'string'
+console.log(typeof String(1));
+
+typeof undefined === 'undefined'
+console.log(typeof undefined);
+
+typeof { a: 1 } === 'object'
+console.log(typeof { a: 1 });
+
+typeof declaredVariable === 'undefined'
+console.log(typeof declaredVariable);
+
+typeof [1, 2, 4] === 'object'
+console.log(typeof [1, 2, 4]);
+
+typeof null === 'object'
+console.log(typeof null);
+
+typeof "John" === 'string'
+console.log(typeof "John");
+
+typeof ([] + '1') === 'string'
+console.log(typeof ([] + '1'));
+
+typeof (1 + '1') === 'string'
+console.log(typeof (1 + '1'));
+
+typeof (10 * '2') === 'number'
+console.log(typeof (10 * '2'));
